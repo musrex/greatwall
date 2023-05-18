3b0d84b3689c3f2d827147ea56774152a1a17921
musrex <leandro.n.lopez@gmail.com>
2023-05-17 23:06:11 -0400

    fix component breaking page

--- main/static/components.js
+++ main/static/components.js
@@ -1,32 +1,3 @@
-import { element } from 'prop-types'
-import React, { useState } from 'react'
-
-const App = () => {
-
-    const [formValues, setFormValues] = useState([{ name: "", email: ""}])
-
-    return (
-        <form onSubmit={handleSubmit}>
-            {formValues.map((element, index) => (
-                div className="form-inline" key={index}
-                <label>Name</label>
-                <input type="text" name="name" value={element.name || ""} onChange={e => handleChange(index, e)} />
-                <label>Email</label>
-                <input type="text" name="email" value={element.email || ""} onChange={e => handleChange(index, e)} />
-                {
-                    index ?
-                        <button type="button" className="button remove" onClick={() => removeFormFields(index)}>Remove</button>
-                    : null  
-                }
-            ))}
-            <div className="button-section">
-                <button className="button add" type="button" onClick={() => addFormFields()}>Add</button>
-                <button className="button submit" type="submit"></button>
-            </div>
-        </form>
-    )
-}
-
 // Select the div element with 'app' id
 const header = document.getElementById('header');
             
